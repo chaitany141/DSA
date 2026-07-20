436bfc04f7807b5abca728eebe4a39d637f7f309
Chaitanya Pawar <157051884+chaitany141@users.noreply.github.com>
2026-07-20 17:13:24 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (19.31%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.cpp
+++ 1386-shift-2d-grid/shift-2d-grid.cpp
@@ -18,15 +18,14 @@ public:
         reverse(arr.begin(), arr.begin() + t);
         reverse(arr.begin() + t, arr.end());
 
-        vector<vector<int>> matrix(m, vector<int>(n));
-
+        
         int idx = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j] = arr[idx++];
+                grid[i][j] = arr[idx++];
             }
         }
 
-        return matrix;
+        return grid;
     }
 };

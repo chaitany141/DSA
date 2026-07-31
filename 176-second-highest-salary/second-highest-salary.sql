5f0fa78840cd9abad465471ca6c0ccad1308c048
Chaitanya Pawar <157051884+chaitany141@users.noreply.github.com>
2026-07-31 22:10:30 +0530

    Time: 320 ms (21.61%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+select(
+    select distinct salary 
+    from Employee
+    order by salary desc
+    LIMIT 1 OFFSET 1
+) as SecondHighestSalary
+    
+
+
+
+
+
+
+
+
+

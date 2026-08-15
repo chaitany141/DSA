4443ebd98e458da2a64572be4ff9674e0dcaf46b
Chaitanya Pawar <157051884+chaitany141@users.noreply.github.com>
2026-08-15 15:43:27 +0530

    Time: 4 ms (10.49%) | Memory: 10.6 MB (79.90%) - LeetSync

---
+++ 338-counting-bits/counting-bits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1);
+
+        for(int i = 0; i <= n; i++){
+            int cnt = 0;
+            for(int j = 0; j<32; j++){
+                if(i & (1 << j)){
+                    cnt++;
+                }
+            }
+            ans[i] = cnt;
+        }
+        return ans;
+    }
+};

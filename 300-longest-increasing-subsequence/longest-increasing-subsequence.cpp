12d3500e537b36352ea2d6f8d2c515477313edb5
Chaitanya Pawar <157051884+chaitany141@users.noreply.github.com>
2026-08-17 21:48:30 +0530

    Time: 346 ms (27.78%) | Memory: 41.7 MB (34.39%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,27 +1,69 @@
 class Solution {
 public:
-int t[2501][2501];
-int n;
-    int solve(vector<int>& arr, int i, int prev){
-        if(i >= n) return 0;
 
-        if(t[i][prev + 1] != -1){
-            return t[i][1 + prev];
-        } 
+int t[2501][2501];
+
+    int solve(int i, int j, vector<int>& arr){
+        int n = arr.size();
+
+        if(j >= n) return 0;
+
+        if(t[i+1][j] != -1) return t[i+1][j];
 
         int take = 0;
-        if(prev == -1 || arr[i] > arr[prev]){
-            take = 1 + solve(arr, i + 1, i);
-        } 
-        int not_take = solve(arr, i + 1, prev); 
 
-        return t[i][prev + 1] = max(take, not_take);
+        if(i == -1 || arr[j] > arr[i]){
+            take = 1 + solve(j, j + 1, arr);
+        }
+
+        int not_take = solve(i, j + 1, arr);
+
+        return t[i+1][j] = max(take, not_take);
+
     }
 
-    int lengthOfLIS(vector<int>& nums) {
+
+    int lengthOfLIS(vector<int>& arr){
         memset(t, -1, sizeof(t));
-        n = nums.size();
 
-        return solve(nums, 0, -1);
+        return solve(-1, 0, arr);
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int t[2501][2501];
+// int n;
+//     int solve(vector<int>& arr, int i, int prev){
+//         if(i >= n) return 0;
+
+//         if(t[i][prev + 1] != -1){
+//             return t[i][1 + prev];
+//         } 
+
+//         int take = 0;
+//         if(prev == -1 || arr[i] > arr[prev]){
+//             take = 1 + solve(arr, i + 1, i);
+//         } 
+//         int not_take = solve(arr, i + 1, prev); 
+
+//         return t[i][prev + 1] = max(take, not_take);
+//     }
+
+//     int lengthOfLIS(vector<int>& nums) {
+//         memset(t, -1, sizeof(t));
+//         n = nums.size();
+
+//         return solve(nums, 0, -1);
+//     }
 };

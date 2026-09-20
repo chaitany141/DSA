436a5662f77939df567b8d64a54e66491698f258
Chaitanya Pawar <157051884+chaitany141@users.noreply.github.com>
2026-09-20 15:43:56 +0530

    Time: 591 ms (44.23%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1161-project-employees-i/project-employees-i.sql
@@ -0,0 +1,12 @@
+
+SELECT p.project_id, 
+ROUND(
+    COALESCE(
+        1.0 * SUM(e.experience_years) / COUNT(p.employee_id), 0 
+    ), 2 
+) AS average_years
+
+FROM Project p
+LEFT JOIN Employee e
+ON p.employee_id = e.employee_id 
+GROUP BY p.project_id
